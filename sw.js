f513425767c30326fbb9b2f6477925f36530a2c4
borakamaci <79101302+KBoraK@users.noreply.github.com>
2026-09-18 23:54:38 +0200

    Remove Firebase module from app cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="endulus-v6.3";
-const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg","./app-v6.js","./firebase-sync.js"];
+const CACHE="endulus-v6.2.4";
+const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg","./app-v6.js"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("endulus-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
