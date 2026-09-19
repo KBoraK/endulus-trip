a90f9afcfdd10fe0bc5562bbf78c025ecb25b012
borakamaci <79101302+KBoraK@users.noreply.github.com>
2026-09-19 17:15:50 +0200

    Bump PWA cache for Málaga Airbnb

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="endulus-v6.2.8";
-const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg","./app-v6.js"];
+const CACHE="endulus-v6.2.9";
+const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg","./app-v6.js?v=6.2.9"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("endulus-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
