6306a757e93c81554c26e478a5149422ad198154
borakamaci <79101302+KBoraK@users.noreply.github.com>
2026-09-19 17:07:00 +0200

    Bump PWA cache for Android nav fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="endulus-v6.2.6";
+const CACHE="endulus-v6.2.7";
 const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg","./app-v6.js"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("endulus-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
