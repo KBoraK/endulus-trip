2626e78d5452f97d78244db7240dafcae7935e8d
borakamaci <79101302+KBoraK@users.noreply.github.com>
2026-09-24 11:59:58 +0200

    Bump dashboard app build to 6.2.22

--- app-v6.js
+++ app-v6.js
@@ -1,6 +1,6 @@
 (()=>{
 "use strict";
-window.ENDULUS_APP_BUILD="6.2.21";
+window.ENDULUS_APP_BUILD="6.2.22";
 document.documentElement.dataset.appBuild=window.ENDULUS_APP_BUILD;
 if(typeof D==="undefined") return;
 const KEY="endulusStateV6";
