8339f5b7582c86939cefd6f1da18d58352439334
borakamaci <79101302+KBoraK@users.noreply.github.com>
2026-09-24 12:33:49 +0200

    Bump app build for city chip fix

--- app-v6.js
+++ app-v6.js
@@ -1,6 +1,6 @@
 (()=>{
 "use strict";
-window.ENDULUS_APP_BUILD="6.2.28";
+window.ENDULUS_APP_BUILD="6.2.29";
 document.documentElement.dataset.appBuild=window.ENDULUS_APP_BUILD;
 if(typeof D==="undefined") return;
 const KEY="endulusStateV6";
