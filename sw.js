32ce3d8637c6383c2ed9be70ce80ee2cc81edfa1
borakamaci <79101302+KBoraK@users.noreply.github.com>
2026-09-24 09:42:41 +0200

    Bump PWA cache after homepage reminder change

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="endulus-v6.2.19";
-const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg","./app-v6.js?v=6.2.19"];
+const CACHE="endulus-v6.2.20";
+const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg","./app-v6.js?v=6.2.20"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("endulus-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
